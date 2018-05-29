e3ccf9da55fb251bec719d1cc7d1aebf28d4a576
yangchd <yangchd@yonyou.com>
2018-05-29 17:53:46 +0800

    Java排序算法

--- src/main/java/com/yangchd/exercise/sort/ShellSort.java
+++ src/main/java/com/yangchd/exercise/sort/ShellSort.java
@@ -7,22 +7,25 @@ package com.yangchd.exercise.sort;
  */
 public class ShellSort {
 
+    /**
+     * i 每组交换后面的数
+     * j 每组交换前面的数
+     * r 每组交换的间距
+     * tmp 交换数据临时存储
+     */
     public void shellSort(int[] arr) {
-        // i表示希尔排序中的第n/2+1个元素（或者n/4+1）
-        // j表示希尔排序中从0到n/2的元素（n/4）
-        // r表示希尔排序中n/2+1或者n/4+1的值
         int i, j, r, tmp;
         // 划组排序
-        for(r = arr.length / 2; r >= 1; r = r / 2) {
-            for(i = r; i < arr.length; i++) {
+        for (r = arr.length / 2; r >= 1; r = r / 2) {
+            for (i = r; i < arr.length; i++) {
                 tmp = arr[i];
                 j = i - r;
                 // 一轮排序
-                while(j >= 0 && tmp < arr[j]) {
-                    arr[j+r] = arr[j];
+                while (j >= 0 && tmp < arr[j]) {
+                    arr[j + r] = arr[j];
                     j -= r;
                 }
-                arr[j+r] = tmp;
+                arr[j + r] = tmp;
             }
         }
     }
